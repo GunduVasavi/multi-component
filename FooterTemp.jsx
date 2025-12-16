bb78e938668d5479277da9f58bcfd83b079fa880
Gundu Vasavi <154065362+GunduVasavi@users.noreply.github.com>
2025-12-16 23:13:48 +0530

    first commit

---
+++ FooterTemp.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        fontSize: "14px",
+        color: "gray",
+        padding: "10px"
+      }}
+    >
+      This is Footer
+    </div>
+  );
+}
+
+export default Footer;
---
+++ MainPage.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from "./NavbarTemp";
+import Content from "./ContentTemp";
+import Footer from "./FooterTemp";
+
+function MainPage() {
+  return (
+    <div>
+      <Navbar />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default MainPage;
